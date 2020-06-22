97712190273b4a3298b2a5f7af885380a86ec3cc
Micki <gsmhirsch@gmail.com>
2020-06-22 20:55:22 +0200

    create table

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.get('/createdb', (req, res) => {
     });
 });
 
+// create table
+app.get('/createtableusers', (req, res) => {
+    let sql = 'CREATE TABLE users(id int AUTO_INCREMENT, user VARCHAR(255), email VARCHAR(255) UNIQUE, password VARCHAR(255), PRIMARY KEY (id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Users table created');
+    })
+});
 
 
 app.listen('3000', () => {
