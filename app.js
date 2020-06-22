bc2683bbf241c82054d97a58c0d2913fcfaaed87
Micki <gsmhirsch@gmail.com>
2020-06-22 20:45:32 +0200

    run | connect |create db through a /route

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const mysql = require('mysql');
 
 const app = express();
 
+//Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'nodemysql'
+});
+
+// Connect
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('MySQL connected...')
+});
+
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created');
+    });
+});
+
+
+
 app.listen('3000', () => {
     console.log('Server running on port 3000');
 });
